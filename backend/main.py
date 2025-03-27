25f276a11657fd577e30f575304a1107ec3d841e
Kevin Serrano <kevinserrano010@gmail.com>
2025-03-27 16:11:12 -0300

    problema de CORS solucionado

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
 import json
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173", # Frontend Svelte
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
+
 @app.get("/data")
 def get_data():
-    with open("./data.json", "r") as file:
+    with open("../data.json", "r") as file:
         data = json.load(file)
     return data
