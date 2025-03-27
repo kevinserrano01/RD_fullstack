9d51f066101fd82399f3b9b93d5996a654d1a511
Kevin Serrano <kevinserrano010@gmail.com>
2025-03-27 16:14:31 -0300

    estructura basica de Svelte con Vite

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+  import DataVisualization from "./home/DataVisualization.svelte";
+</script>
+
+<div class="container">
+  <h1>Visualizacion de Datos con Gráficos</h1>
+  <DataVisualization />
+</div>
+
+<style>
+  .container {
+    font-family: sans-serif;
+    margin: 0;
+    padding: 0;
+    background-color: #e3e3e3;
+    border-radius: 40px;
+  }
+
+  h1 {
+    text-align: center;
+    padding: 20px;
+  }
+</style>
